7e1bfc8f009f8123898d2ee86b9f0f7ecbd27b6a
laurent chastain <laurent.chastain@ordicollege.fr>
2016-07-14 18:56:17 +0200

    Add files via upload

---
+++ decoder_remoteIR.ino
@@ -0,0 +1,31 @@
+#include <IRremote00.h>
+
+#define DELAI 200
+
+   #define RECV_PIN 3   //recepteur ir sur entrée 3 numérique
+
+   IRrecv irrecv(RECV_PIN);
+   decode_results results;
+
+   const int LED = 13; //led sur pin13
+
+
+   void setup()
+   {
+     pinMode(LED, OUTPUT);
+     irrecv.enableIRIn(); // Start receiver
+     Serial.begin(9600);
+   }
+
+
+   void loop() {
+       digitalWrite(LED, LOW);
+  
+     // si message ir
+   if (irrecv.decode(&results)) {
+   Serial.println(results.value, HEX); //ecrire code
+   digitalWrite(LED, HIGH); //allumer led
+   delay(DELAI);
+   irrecv.resume(); // attente prochaine commande
+     }
+   }

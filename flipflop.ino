8595422a026abc1664c8632f1cf8844e726ce2e7
laurent chastain <laurent.chastain@ordicollege.fr>
2016-07-15 21:42:20 +0200

    Add files via upload

---
+++ flipflop.ino
@@ -0,0 +1,29 @@
+boolean __ardublockDigitalRead(int pinNumber)
+{
+  pinMode(pinNumber, INPUT);
+  return digitalRead(pinNumber);
+}
+
+
+
+void setup()
+{
+  pinMode( 8 , OUTPUT);
+  pinMode( 7 , OUTPUT);
+}
+
+void loop()
+{
+  if (__ardublockDigitalRead(2))
+  {
+    digitalWrite(8 , HIGH);
+    digitalWrite(7 , LOW);
+  }
+  else
+  {
+    digitalWrite(8 , LOW);
+    digitalWrite(7 , HIGH);
+  }
+}
+
+

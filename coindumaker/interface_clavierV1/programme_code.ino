48c0302d6fd6757838f974c135149d7d54d4e59b
laurent chastain <laurent.chastain@ordicollege.fr>
2016-08-02 20:18:04 +0200

    Add files via upload

---
+++ coindumaker/interface_clavierV1/programme_code.ino
@@ -0,0 +1,27 @@
+#include <Keypad.h>
+
+//libraries at http://duinoedu.com/dl/lib/grove/EDU_Keypad_Dupont/ 
+Keypad keypad;
+
+void setup()
+{
+  keypad.brancher(2,8);
+  keypad.activerInfos();
+  keypad.writePassword("1234");
+  pinMode( 13 , OUTPUT);
+}
+
+void loop()
+{
+  if (keypad.testPasswordOk())
+  {
+    digitalWrite(13 , HIGH);
+    delay( 5000 );
+  }
+  else
+  {
+    digitalWrite(13 , LOW);
+  }
+}
+
+
---
+++ coindumaker/interface_clavierV1/programme_lcd.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+#include <rgb_lcd.h>
+
+boolean __ardublockDigitalRead(int pinNumber)
+{
+  pinMode(pinNumber, INPUT);
+  return digitalRead(pinNumber);
+}
+
+
+//libraries at http://duinoedu.com/dl/lib/grove/EDU_LCD_RGB_Backlight_Grove/ 
+rgb_lcd monRgb;
+
+void setup()
+{
+  monRgb.branch();
+}
+
+void loop()
+{
+  if (__ardublockDigitalRead(2))
+  {
+    monRgb.placerCurseurEn(0,0);
+    monRgb.ecrire("acces autorise" );
+  }
+  else
+  {
+    monRgb.placerCurseurEn(0,0);
+    monRgb.ecrire("saisir code" );
+  }
+}
+
+

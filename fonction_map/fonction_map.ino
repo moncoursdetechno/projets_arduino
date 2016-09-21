9977495d58433bc0e0ac92a6b55622376d1d4d7b
laurent chastain <laurent.chastain@ordicollege.fr>
2016-09-21 21:25:05 +0200

    Add files via upload

---
+++ fonction_map/fonction_map.ino
@@ -0,0 +1,14 @@
+int _ABVAR_1_potar = 0 ;
+
+void setup()
+{
+  pinMode( 3 , OUTPUT);
+}
+
+void loop()
+{
+  _ABVAR_1_potar = analogRead(A0) ;
+  analogWrite(3 , map ( _ABVAR_1_potar , 0 , 1023 , 0 , 255 ) );
+}
+
+

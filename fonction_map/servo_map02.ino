6ea27f48a716f8fb9eec3f5bfa39ad5c74ffd661
laurent chastain <laurent.chastain@ordicollege.fr>
2016-09-21 21:12:01 +0200

    Add files via upload

---
+++ fonction_map/servo_map02.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+
+int _ABVAR_1_potar = 0 ;
+Servo servo_pin_3;
+
+void setup()
+{
+  servo_pin_3.attach(3);
+}
+
+void loop()
+{
+  _ABVAR_1_potar = analogRead(A0) ;
+  servo_pin_3.write( map ( _ABVAR_1_potar , 0 , 1023 , 0 , 180 )  );
+}
+
+
